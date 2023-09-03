b35b2790260f6c0c699ed1d4c9eec45f0287a562
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-09-03 18:40:02 +0530

    Board UI ready

--- src/main/kotlin/Chess.kt
+++ src/main/kotlin/Chess.kt
@@ -1,9 +1,13 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -22,7 +26,7 @@ private fun ChessSquare(
     )
 }
 
-const val BOARD_SIZE = 8
+const val BOARD_SIZE = 7
 
 @Composable
 fun ChessBoard() {
@@ -35,19 +39,82 @@ fun ChessBoard() {
                 .size(size)
                 .padding(16.dp),
         ) {
+            Row {
+                Box(
+                    modifier = Modifier
+                        .width(24.dp)
+                )
+                for (i in 'A'..'A' + BOARD_SIZE) {
+                    Text(
+                        text = "$i",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .rotate(180f),
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .width(24.dp)
+                )
+            }
             for (i in 0..BOARD_SIZE) {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .weight(1f)
+                        .weight(1f),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .width(24.dp)
+                    ) {
+                        Text(
+                            "${BOARD_SIZE - i + 1}",
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                        )
+                    }
                     for (j in 0..BOARD_SIZE) {
                         ChessSquare(
                             isWhite = (i + j) % 2 == 1,
                             modifier = Modifier.weight(1f)
                         )
                     }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .width(24.dp)
+                    ) {
+                        Text(
+                            "${BOARD_SIZE - i + 1}",
+                            modifier = Modifier
+                                .rotate(180f)
+                                .align(Alignment.Center)
+                        )
+                    }
                 }
+            }
+            Row {
+                Box(
+                    modifier = Modifier
+                        .width(24.dp)
+                )
+                for (i in 'A'..'A' + BOARD_SIZE) {
+                    Text(
+                        text = "$i",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f),
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .width(24.dp)
+                )
             }
         }
     }
